6ad986cf582e7ff267051bc7d856db725d48cf4b
dgtalco <nvestinc@gmail.com>
2021-03-06 12:27:45 -0600

    2nd commit

---
+++ src/program1_test/TestGitHub1.java
@@ -0,0 +1,12 @@
+package program1_test;
+
+public class TestGitHub1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello GitHub");
+        System.out.println("Hi");
+        System.out.println("Hello again");
+        //notes
+
+    }
+}
