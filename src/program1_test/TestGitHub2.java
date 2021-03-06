76db82a1ee28a643e05e62cb6eae71f348cd3aae
dgtalco <nvestinc@gmail.com>
2021-03-06 12:21:57 -0600

    1st commit

--- src/program1_test/TestGitHub2.java
+++ src/program1_test/TestGitHub2.java
@@ -1,4 +1,10 @@
 package program1_test;
 
-public class TestGitHub2 {
+public class TestGitHub2 {;
+
+    public static void main(String[] args) {
+        //good project
+        //hello hello
+
+    }
 }
